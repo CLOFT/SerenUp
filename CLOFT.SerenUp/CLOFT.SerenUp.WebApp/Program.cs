667defc3596e8e55bdb87f391eca2c785545dece
Stefan Mihalache <ste.mihalache@gmail.com>
2022-06-23 16:32:45 +0200

    Add project

--- CLOFT.SerenUp/CLOFT.SerenUp.WebApp/Program.cs
+++ CLOFT.SerenUp/CLOFT.SerenUp.WebApp/Program.cs
@@ -1,20 +1,26 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using CLOFT.SerenUp.WebApp.Data;
+using Amazon.Extensions.NETCore.Setup;
+using Amazon.Runtime;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+AWSOptions awsOptions = new AWSOptions
+{
+    Credentials = new BasicAWSCredentials(builder.Configuration.GetConnectionString("accessKey"), builder.Configuration.GetConnectionString("secretKey"))
+};
+builder.Services.AddDefaultAWSOptions(awsOptions);
 
+
+//Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddCognitoIdentity();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -26,8 +32,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-//app.UseAuthentication();
-//app.UseAuthorization();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapRazorPages();
 
